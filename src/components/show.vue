e5df7d0311962bd987d6dd67c69aa7b1fe896efe
1140711243 <1140711243@qq.com>
2020-11-09 19:34:38 +0800

    modify

---
+++ src/components/show.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-carousel height="410px" id="show" >
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small"> </h3>
+      </el-carousel-item>
+      <div class="Test" >
+      </div>
+    </el-carousel>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url('../assets/css/show.css');
+</style>
